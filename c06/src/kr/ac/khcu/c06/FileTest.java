de147453cb1f386d01fb9f53b31d8483bf70127f
kenu <kenu.heo@gmail.com>
2014-01-27 09:00:52 +0900

    refactor

--- c06/src/kr/ac/khcu/c06/FileTest.java
+++ c06/src/kr/ac/khcu/c06/FileTest.java
@@ -1,16 +1,27 @@
 package kr.ac.khcu.c06;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class FileTest {
-	public static void main(String[] args) throws IOException {
-	      BufferedReader in = new BufferedReader(new FileReader("README.md"));
-	      String s;
-	      while((s = in.readLine()) != null) {
-	    	  System.out.println(s);
-	      }
-	      in.close();
+	public static void main(String[] args) throws FileNotFoundException {
+		try {
+			read();
+		} catch (FileNotFoundException e) {
+			throw e;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void read() throws FileNotFoundException, IOException {
+		BufferedReader in = new BufferedReader(new FileReader("README.md"));
+		String s;
+		while ((s = in.readLine()) != null) {
+			System.out.println(s);
+		}
+		in.close();
 	}
 }
